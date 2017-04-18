28122f76def2d0076ec0bdd840953c8661a4dee4
yeyong <yey@5mutian.com>
2017-04-18 15:43:57 +0800

    提现

--- src/components/page/Encashs.vue
+++ src/components/page/Encashs.vue
@@ -8,11 +8,23 @@
            </router-link>
            </el-breadcrumb-item>
 
-        <el-breadcrumb-item><i class="el-icon-setting"></i>提现列表</el-breadcrumb-item>
-        </el-breadcrumb>
+           <el-breadcrumb-item><i class="el-icon-setting"></i>提现列表</el-breadcrumb-item>
+           </el-breadcrumb>
     </div>
+    <div class="tag-ele">
+      <span @click="fetch_type('alipay')">
+        <el-tag class="tag" :class="type === 'alipay' ? type : null">提现到支付宝</el-tag>
+      </span>
+      <span @click="fetch_type('wx_pub')">
+        <el-tag type="gray" class="tag" :class="type === 'wx_pub' ? type : null">提现到微信公众号</el-tag>
+      </span>
+      <span @click="fetch_type('bank')">
+        <el-tag type="primary" class="tag" :class="type === 'bank' ? type : null">提现到银行卡</el-tag>
+      </span>
+    </div>
+    <hr >
     <el-table :data="encashs" border>
-      <el-table-column label="生成时间" width="200">
+      <el-table-column label="生成时间" width="300">
         <template scope="scope">
           {{scope.row.created_at * 1000 | date("%Y-%m-%d %T")}}
         </template>
@@ -27,11 +39,13 @@
               <span @click="show_users(scope.row.id)" class="el-button el-button--primary">查看明细</span>
             </template>
             </el-table-column>
+            <template v-if="type === 'alipay'">
             <el-table-column label="操作">
               <template scope="scope">
-                <span @click="batch_pay(scope.row.id)" class="el-button el-button--danger">提交到支付宝</span>
+                <span @click="batch_pay(scope.row.id)" class="el-button el-button--danger">提交到{{type | conver}}</span>
               </template>
               </el-table-column>
+            </template>
               </el-table>
 
               <el-dialog title="提现的用户" v-model="dialogVisible" size="large">
@@ -49,6 +63,14 @@
                         {{scope.row.created_at * 1000 | date("%Y-%m-%d %T")}}
                       </template>
                       </el-table-column>
+                      <template v-if="type != 'alipay'">
+                        <el-table-column label="操作">
+                          <template scope="scope">
+                            <span @click="single_handle(scope.row.id, scope.$index)" class="el-button el-button--danger">处理{{type | conver}}支付</span>
+                          </template>
+                          </el-table-column>
+
+                      </template>
                       </el-table>
                       </el-dialog>
   </div>
@@ -59,15 +81,36 @@ export default {
     return {
       encashs: [],
       dialogVisible: false,
-      users: []
+      users: [],
+      type: "alipay",
+      en_index: 0
     }
   },
   created(){
-    this.fetch_soruce()
+    this.fetch_soruce(this.type)
+  },
+  filters: {
+    conver(val) {
+      let v = "";
+      switch(val) {
+        case "alipay":
+          v = "支付宝";
+          break;
+        case "wx_pub":
+          v = "公众号"
+          break;
+        case "bank":
+          v = "银行卡"
+          break;
+        default:
+          v = "支付宝";
+      }
+      return v
+    }
   },
   methods: {
     fetch_soruce() {
-      this.$http.get("encashments.json").then(res => {
+      this.$http.get("encashments.json?type=" + this.type).then(res => {
         let data = res.body;
         this.encashs = data.klass;
       }, res => {
@@ -91,7 +134,45 @@ export default {
       }, res => {
         this.$alert("加载失败");
       })
-    }
+    },
+      fetch_type(type) {
+        this.type = type
+        this.fetch_soruce(type)
+      },
+      single_handle(id, index) {
+        this.$http.post("encashments/" + id + "/recipient.json", {type: this.type}).then(res => {
+          let data = res.body
+          if (data.code == 200) {
+            this.users.splice(index, 1);
+            this.$message("处理完成");
+            if (this.users.length <= 0) {
+              this.$http.post("encashments/" + id + "/close_encash.json", {}).then(res =>{
+                if (res.body.code == 200) {
+                  this.encashs.splice(0, 1);
+                  this.dialogVisible = false;
+                }
+              })
+            }
+          }
+        })
+      }
   }
 }
 </script>
+<style scoped>
+hr {
+  margin: 15px 0;
+}
+.tag {
+  margin-right: 5px;
+  width: 20%;
+  text-align: center;
+  padding: 12px;
+  height: 50px;
+  font-size: 16px;
+  cursor: pointer;
+}
+.alipay, .wx_pub, .bank {
+  border-bottom: 4px solid red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'element-ui/lib/theme-default/index.css';    // 默认主题
 Vue.use(ElementUI);
 Vue.use(VueResoure);
 Vue.use(vueFilter)
-Vue.http.options.root = "https://api.chuanggj.com/admin";
+Vue.http.options.root = "http://127.0.0.1:3000/admin";
 Vue.http.options.emulateJSON = true;
 Vue.http.options.emulateHTTP = true;
 Vue.http.interceptors.push(function(request, next){
