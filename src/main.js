fe387ecf04ea7487ba7ea347eb0c2d920adeef67
yeyong <yey@5mutian.com>
2017-05-11 12:27:49 +0800

    man

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ import 'element-ui/lib/theme-default/index.css';    // 默认主题
 Vue.use(ElementUI);
 Vue.use(VueResoure);
 Vue.use(vueFilter)
-Vue.http.options.root = "https://api.chuanggj.com/admin";
+//Vue.http.options.root = "https://api.chuanggj.com/admin";
+Vue.http.options.root = "http://192.168.0.164:8080/admin";
 //Vue.http.options.root = "http://localhost:3000/admin";
 Vue.http.options.emulateJSON = true;
 Vue.http.options.emulateHTTP = true;
